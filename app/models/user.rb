179028522a81c763a53450cfc30f5940e1b8e705
Kamil Sobieraj <ksobej@gmail.com>
2011-08-29 23:56:21 +0100

    changed standard text edit of FormTemplate.contents with CKEditor

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   
   has_and_belongs_to_many :roles
+  
+  def role?(role)
+    return !!self.roles.find_by_name(role.to_s.camelize)
+  end
 end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,3 +1,15 @@
 RailsAdmin.config do |c|
   c.authorize_with :cancan
+  
+  c.model FormTemplate do
+    edit do
+      field :name, :string do
+      end
+      field :description, :text do
+      end
+      field :contents, :text do
+        ckeditor true
+      end
+    end
+  end
 end
