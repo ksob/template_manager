7c754ab0d8e5996a4814c8c382a06eaa70a3eff0
Kamil Sobieraj <ksobej@gmail.com>
2011-09-20 18:16:58 +0100

    remove Test::Unit as resolved to use Cucumber, RSpec and Factory Girl instead

--- test/unit/filled_form_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FilledFormTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/form_template_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FormTemplateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
